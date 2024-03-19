2899ce72cb2800398fb082cd2282758082ca9543
Clayton  Brochier <99225232+Clayton-Brochier@users.noreply.github.com>
2024-03-19 16:25:04 -0300

    Seletores DOM

---
+++ pasta/seletores-dom/script.js
@@ -0,0 +1,14 @@
+console.log("Olá mundo!")
+
+const titulo = document.getElementById("titulo")
+console.log(titulo)
+
+const textos = document.getElementsByClassName("texto-simples")
+console.log(textos)
+
+const segundoTitulo = document.querySelector("div h2")
+console.log(segundoTitulo)
+
+const textoPorClasse = document.querySelectorAll(".texto-simples")
+console.log(textoPorClasse)
+
